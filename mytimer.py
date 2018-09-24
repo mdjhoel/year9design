171250496253c0dbeabd917b85d6e5db3c62213f
mdjhoel <mdjhoel@gmail.com>
2018-09-24 15:13:25 -0400

    add files

---
+++ mytimer.py
@@ -0,0 +1,25 @@
+''' 
+mytimer.py - a program to test the speed of your computer
+mhoel - original coding, sept. 21, 2018
+'''
+
+# import timeit module
+# only import the function required - speed up your code
+from timeit import default_timer as timer
+
+# defining a function called mytimer
+def mytimer():
+
+    start = timer()
+    print(start)
+	
+    for i in range(1000000):
+		print(i)
+		
+    end = timer()
+    print(end)
+    
+    print(end - start)
+
+# calling or invoking the function mytimer	
+mytimer()
